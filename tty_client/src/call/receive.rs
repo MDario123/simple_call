17d53e62ab37dca0fdc35d9e295b3a27a4c8a038
MDario123 <manuel.dario.oliver@gmail.com>
2025-06-15 20:49:43 +0200

    client: change opus for a fork that has an updated sys dependency

--- tty_client/src/call/receive.rs
+++ tty_client/src/call/receive.rs
@@ -4,6 +4,16 @@ use cpal::OutputCallbackInfo;
 
 use super::FRAME_SIZE;
 
+fn bytes_human_readable(bytes: usize) -> String {
+    if bytes < 1024 {
+        format!("{} B", bytes)
+    } else if bytes < 1024 * 1024 {
+        format!("{:.2} KB", bytes as f32 / 1024.0)
+    } else {
+        format!("{:.2} MB", bytes as f32 / (1024.0 * 1024.0))
+    }
+}
+
 pub(crate) fn create_speaker_callback(
     udp_sock: UdpSocket,
 ) -> impl FnMut(&mut [f32], &OutputCallbackInfo) {
@@ -20,6 +30,10 @@ pub(crate) fn create_speaker_callback(
 
     let mut last_id: u8 = 0;
 
+    let mut bytes_received: usize = 0;
+
+    let instant = std::time::Instant::now();
+
     move |mut data: &mut [f32], _: &OutputCallbackInfo| {
         while !data.is_empty() {
             // Copy all that's possible from out_buff to data
@@ -31,6 +45,15 @@ pub(crate) fn create_speaker_callback(
 
             if out_buff_filled_l == FRAME_SIZE {
                 if let Ok((size, _)) = udp_sock.recv_from(&mut recv_buff) {
+                    bytes_received += size + 24;
+                    let elapsed = instant.elapsed();
+                    println!(
+                        "Packet {}. Size: {}. Bytes received: {}. Elapsed: {:.2?} seconds.",
+                        recv_buff[0],
+                        size,
+                        bytes_human_readable(bytes_received),
+                        elapsed,
+                    );
                     if recv_buff[0] != last_id.wrapping_add(1) {
                         println!(
                             "Packet ID mismatch: expected {}, got {}.",
@@ -45,6 +68,14 @@ pub(crate) fn create_speaker_callback(
                         .decode_float(&recv_buff[1..size], &mut out_buff, false)
                         .unwrap();
                 } else {
+                    let elapsed = instant.elapsed();
+                    println!(
+                        "Packet {}. Size: {}. Bytes received: {}. Elapsed: {:.2?} seconds.",
+                        last_id,
+                        0,
+                        bytes_human_readable(bytes_received),
+                        elapsed,
+                    );
                     last_id = last_id.wrapping_add(1);
                     out_buff_filled_l = 0;
                     decoder.decode_float(&[], &mut out_buff, false).unwrap();
